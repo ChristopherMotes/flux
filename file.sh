c3110c11a2c5a3948e46f7e5de86e0f0ad643784
EC2 Default User <ec2-user@ip-172-31-38-243.us-east-2.compute.internal>
2018-06-12 15:37:09 +0000

    some more clean up

--- file.sh
+++ file.sh
@@ -2,14 +2,14 @@
 attempt=0
 primeNumber=0
 while primeNumber < 6:
-    print attempt
+    print "prime Number top of loop " + str(primeNumber)
     testValue = 2
     while testValue <= 9:
-        printStatement = "test value" + str(testValue)
-        print printStatement
-        if attempt % testValue == 0:
+        if attempt % testValue != 0:
+            print "I found Not Prime"
+        else:
             primeNumber+=1
         testValue+=1
     print "primeNumber Loop " +  str(primeNumber)
     attempt+=1
-print primeNumber
+print attempt
