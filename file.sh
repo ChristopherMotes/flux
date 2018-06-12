a18af7eded93c86b1b508078b64cfa2c1bdc9463
EC2 Default User <ec2-user@ip-172-31-38-243.us-east-2.compute.internal>
2018-06-12 15:01:08 +0000

    where we ended

---
+++ file.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+attempt=0
+primeNumber=0
+while primeNumber != 6:
+    print attempt
+    testValue = 2
+    while testValue != 9:
+        printStatement = "test value" + str(testValue)
+        print printStatement
+        if attempt % testValue == 0:
+            primeNumber+=primeNumber
+        testValue+=1
+    attempt+=attempt
+print primeNumber
