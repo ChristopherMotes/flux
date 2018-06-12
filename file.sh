952cbfc4b08ecd0db8379ee6e05e961ce682f386
EC2 Default User <ec2-user@ip-172-31-38-243.us-east-2.compute.internal>
2018-06-12 15:16:32 +0000

    while loop should be less than six

--- file.sh
+++ file.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 attempt=0
 primeNumber=0
-while primeNumber != 6:
+while primeNumber < 6:
     print attempt
     testValue = 2
     while testValue <= 9:
         printStatement = "test value" + str(testValue)
         print printStatement
-        print "mod value " + str(attempt % testValue == 0)
         if attempt % testValue == 0:
             primeNumber+=1
         testValue+=1
+    print "primeNumber Loop " +  str(primeNumber)
     attempt+=1
 print primeNumber
